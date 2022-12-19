a1868eb63e67397fcb37f90252361c6b9f4ae4c9
naliekkez <jayaharsonoeka@yahoo.com>
2022-12-20 02:12:04 +0700

    Update merkle_tree.cpp

--- merkle_tree.cpp
+++ merkle_tree.cpp
@@ -13,7 +13,7 @@ typedef struct node{
 class hashTree{
 	
 	struct node *merkleRoot;
-	
+	// simple hash function which will calculate based on the char and it's position. with position as a magnitude
 	int hash_function(string s){
 		int sum = 0;
 		for(int i = 1; i <= s.length();i++){
@@ -23,7 +23,7 @@ class hashTree{
 	}	
 	struct node*  buildTree(vector< struct node*> nodes){
 		
-		if(nodes.size()%2 == 1){
+		if(nodes.size()%2 == 1){ // if the element is not even, we make a duplicate pair.
 			nodes.push_back(nodes[nodes.size()-1]);
 		}
 		
@@ -31,26 +31,27 @@ class hashTree{
 		n = (struct node*) malloc(sizeof(struct node));
 		
 		string temp("");
-		if(nodes.size() == 2){
+		if(nodes.size() == 2){ // if there is exactly 2 element, then create a new parent node with those two as it's child
 			n->left = nodes[0];
 			n->right = nodes[1];
 			temp+=nodes[0]->data;
 			temp+=nodes[1]->data;
 			strcpy(n->data,temp.c_str());
 			n->value = hash_function(temp);
+			// return the set of the tree (BST)
 			return n;
 		}
-		int half_idx = nodes.size()/2;
+		int half_idx = nodes.size()/2; // if not, divide and conquer, find half point
 		
-	    vector<struct node*> first_half(nodes.begin(), nodes.begin()+half_idx);
-	    vector<struct node*> second_half(nodes.begin()+half_idx,nodes.end());
-		n->left =  buildTree(first_half);
-		n->right =  buildTree(second_half);
-		temp+=n->left->data;
-		temp+=n->right->data;
+	    vector<struct node*> first_half(nodes.begin(), nodes.begin()+half_idx); // get the first half
+	    vector<struct node*> second_half(nodes.begin()+half_idx,nodes.end()); // get the second half
+		n->left =  buildTree(first_half); // set the left node with the root from the first half element
+		n->right =  buildTree(second_half); // set the left node with the root from the second half element
+		temp+=n->left->data; // construct the string data
+		temp+=n->right->data; // construct the string data
 		
 		strcpy(n->data,temp.c_str());
-		n->value = hash_function(temp);
+		n->value = hash_function(temp); // construct the hash value based on data.
 		return n;
 		
 	}
@@ -83,8 +84,6 @@ class hashTree{
 
 int main(){
 	
-	//freopen("file.in","r",stdin); // ini untuk redirect semua input dari file.in ke scanf
-	//freopen("file.out","w",stdout); // ini untuk redirect semua printf ke file.out
 	vector<string> transactions;
 	transactions.push_back("aaa");
 	transactions.push_back("bbb");
